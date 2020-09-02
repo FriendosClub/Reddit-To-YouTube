bef2cf8bf8fc2e56730b61afef7f6f90efcf7a1b
Ralph Drake <rama.o.ralph+github@gmail.com>
2020-09-02 02:27:31 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Reddit-To-YouTube
+
+Reddit-To-YouTube scrapes a subreddit for vreddit videos, combines them into
+one monolithic video, and uploads them to YouTube.
---
+++ reddit-to-youtube.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+## See the bottom of this file for license information
+
+import ffmpeg, requests, youtube_dl
+
+# more code here
+
+
+#  Reddit-To-YouTube combines vreddit posts into one YouTube video.
+#  Copyright (C) 2020  Calvin Barrett, Ralph Drake
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
